95dcedcb4397eb3182923d0395790090c28f17b2
sunwoo162 <sunwoosunwoo4114@gmail.com>
2026-08-20 17:33:17 +0900

    feat : register contest command

--- src/register-commands.ts
+++ src/register-commands.ts
@@ -1,4 +1,5 @@
 import { REST, Routes } from "discord.js";
+import { contestCommand } from "./commands/contest.js";
 import { config } from "./config.js";
 import { projectCommand } from "./commands/project.js";
 
@@ -7,7 +8,7 @@ const rest = new REST({ version: "10" }).setToken(config.discordToken);
 await rest.put(
   Routes.applicationGuildCommands(config.discordClientId, config.discordGuildId),
   {
-    body: [projectCommand.toJSON()],
+    body: [projectCommand.toJSON(), contestCommand.toJSON()],
   },
 );
 
