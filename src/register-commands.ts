f186300a616278bf07c0f56b5beb4efd8d906329
sunwoo162 <sunwoosunwoo4114@gmail.com>
2026-08-25 09:30:48 +0900

    chore: remove job slash command registration

--- src/register-commands.ts
+++ src/register-commands.ts
@@ -1,6 +1,5 @@
 import { REST, Routes } from "discord.js";
 import { contestCommandV2 } from "./commands/contest-v2.js";
-import { jobCommand } from "./commands/job.js";
 import { config } from "./config.js";
 import { musicCommand } from "./commands/music.js";
 import { projectCommand } from "./commands/project.js";
@@ -10,7 +9,6 @@ const rest = new REST({ version: "10" }).setToken(config.discordToken);
 const commands = [
   projectCommand.toJSON(),
   contestCommandV2.toJSON(),
-  jobCommand.toJSON(),
   voiceCommand.toJSON(),
   musicCommand.toJSON(),
 ];
